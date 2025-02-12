886eb5a2898459357368d918cfa04504a27b0630
komorebi <18513615864@163.com>
2025-02-12 18:59:26 +0800

    1

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,17 @@
+import { useLocalSearchParams } from "expo-router";
+import React, { FC } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface IProps {}
+
+ const Property: FC<IProps> = (props) => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>Property {id}</Text>
+    </View>
+  );
+};
+
+
+export default Property;
