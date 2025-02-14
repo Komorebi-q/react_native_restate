2fa840477354a2e319cf06f0f87bba735c9cea30
komorebi_qkc <komorebi_qkc@163.com>
2025-02-14 08:59:35 +0800

    3

--- app/(root)/(tab)/index.tsx
+++ app/(root)/(tab)/index.tsx
@@ -1,15 +1,17 @@
+import { Card, FeatureCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import { useGlobalContext } from "@/lib/global-provider";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const { user } = useGlobalContext();
   return (
     <SafeAreaView className="h-full bg-white ">
-      <View className="px-5">
+      <ScrollView className="h-full" contentContainerClassName="px-5">
         <View className="flex flex-row items-center justify-between mt-5">
           <View className="flex flex-row items-center">
             <Image
@@ -25,12 +27,10 @@ export default function Index() {
               </Text>
             </View>
           </View>
-        <Image source={icons.bell} className="size-6" />
-
+          <Image source={icons.bell} className="size-6" />
         </View>
 
         <Search />
-
 
         <View className="my-5">
           <View className="flex flex-row items-center justify-between">
@@ -44,7 +44,32 @@ export default function Index() {
             </TouchableOpacity>
           </View>
         </View>
-      </View>
+
+        <View className="flex flex-row gap-5 mt-5">
+          <FeatureCard />
+          <FeatureCard />
+        </View>
+
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Our Recommendation
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <Filters />
+
+          <View className="flex flex-row gap-5 mt-5">
+            <Card />
+            <Card />
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
